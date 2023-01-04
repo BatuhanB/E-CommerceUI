4288a4e45872957ec8e7eb9060abf4c67b0156cb
BatuhanB <balibatuhan19@gmail.com>
2023-01-04 20:56:26 +0300

    sidebar and admin navigation style has been added

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,6 +2,7 @@ import { ComponentsModule } from './layout/components/components.module';
 import { LayoutModule } from './layout/layout.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 
--- src/app/admin/layout/layout.module.ts
+++ src/app/admin/layout/layout.module.ts
@@ -3,6 +3,7 @@ import { ComponentsModule } from './components/components.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 
@@ -14,7 +15,8 @@ import { LayoutComponent } from './layout.component';
     imports: [
         CommonModule,
         ComponentsModule,
-        RouterModule
+        RouterModule,
+        MatSidenavModule
     ],
     exports:[
       LayoutComponent
